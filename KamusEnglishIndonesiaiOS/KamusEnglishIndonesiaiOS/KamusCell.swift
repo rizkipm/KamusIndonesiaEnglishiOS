ae26359261909d7340baa739d5bef60b4e66b665
Programmer Muslim <rizki@imastudio.co.id>
2017-11-09 10:28:59 +0700

    add table view

---
+++ KamusEnglishIndonesiaiOS/KamusEnglishIndonesiaiOS/KamusCell.swift
@@ -0,0 +1,24 @@
+//
+//  KamusCell.swift
+//  KamusEnglishIndonesiaiOS
+//
+//  Created by Rizki Syaputra on 11/9/17.
+//  Copyright © 2017 Rizki Syaputra. All rights reserved.
+//
+
+import UIKit
+
+class KamusCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
